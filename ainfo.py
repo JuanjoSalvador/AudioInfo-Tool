93f09b13d03fa75cba7e2095d994910f6f934698
JuanjoSalvador <juanjosalvador@openmailbox.org>
2016-11-24 19:23:39 +0100

    check if input file is audio

--- ainfo.py
+++ ainfo.py
@@ -1,5 +1,18 @@
 from mutagen.mp3 import MP3
 import sys
+import magic
+
+# Return true if is an audio file
+def isAudio(file):
+    import magic
+    mime = magic.Magic(mime=True)
+    mimetype = mime.from_file(file)
+    isAudio = False
+    typeOfFile = mimetype.split("/")
+    if typeOfFile[0] == "audio":
+        isAudio = True
+    
+    return isAudio
 
 def technical(a):
     audio = MP3(a)
@@ -14,7 +27,24 @@ def technical(a):
 def full():
     print "Not implemented yet."
 
-if sys.argv[1] == "-t":
-    technical(sys.argv[2])
+def needHelp():
+    print "Do you need help? Try ainfo --help"
+
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "-t":
+        if isAudio(sys.argv[2]):
+            technical(sys.argv[2])
+        else:
+            print sys.argv[2], "is not an valid file."
+
+    elif len(sys.argv) > 1 and sys.argv[1] == "-f":
+        if isAudio(sys.argv[2]):
+            full()
+        else:
+            print sys.argv[2], "is not an valid file."
+
+    else:
+        needHelp()
 else:
-    full()
+    needHelp()
